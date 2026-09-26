1d41ea253ee81580c56494936420aa24a49d6988
vegeta0911 <67876957+vegeta0911@users.noreply.github.com>
2026-09-26 17:15:44 +0200

    Fix labels and titles in configuration form

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -24,7 +24,7 @@ if (!isConnect()) {
 ?>
 <form class="form-horizontal">
     <fieldset>
-        <legend><i class="fas fa-charging-station"></i> {{Configuration générale}}</legend>
+        <legend><i class="fas fa-charging-station"></i>{{Configuration générale}}</legend>
         <div class="form-group">
             <label class="col-sm-4 control-label">{{Fréquence de rafraîchissement par défaut}}
                 <sup><i class="fas fa-question-circle floatright" title="{{Intervalle en secondes utilisé par défaut pour interroger chaque chargeur, sauf si un intervalle spécifique est défini sur l'équipement.}}"></i></sup>
@@ -37,8 +37,8 @@ if (!isConnect()) {
             </div>
         </div>
         <div class="form-group">
-            <label class="col-sm-4 control-label">{{Commande statue batterie}}
-			    <sup><i class="fas fa-question-circle floatright" title="{{Mettre la commande de pourcentage de la batterie sur la voiture electrique.}}"></i></sup>
+            <label class="col-sm-4 control-label">{{Commande status batterie}}
+			    <sup><i class="fas fa-question-circle floatright" title="{{Mettre la commande de pourcentage de la batterie de votre voiture électrique.}}"></i></sup>
 			</label>
 			<div class="col-sm-3">
                 <div class="input-group">
